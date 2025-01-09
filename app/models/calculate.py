751c9792a5c0c600d97874889a363c76122ba6b1
Miguel Fernandez <miguelferlez@gmail.com>
2025-01-09 22:09:17 +0100

    test: Calculate class properties and testing

--- app/models/calculate.py
+++ app/models/calculate.py
@@ -1,14 +1,56 @@
 from enum import Enum
 
-class Operation(Enum):
-    ADD = lambda x, y: x + y
-    SUB = lambda x, y: x - y
-    MUL = lambda x, y: x * y
-    DIV = lambda x, y: x / y
+class Status(Enum):
+    EMPTY = 0
+    PARTIAL = 1
+    PENDING = 2
+    COMPLETE = 3
+    FINISHED = 4
 
+class Operation(Enum):
+    ADD = (lambda x, y: x + y, )
+    SUB = (lambda x, y: x - y, )
+    MULT = (lambda x, y: x * y, )
+    DIV = (lambda x, y: x / y, )
+    MOD = (lambda x, y: x % y)
+
+    def __init__(self, operation):
+        self.op = operation
+
+    def calculate(self, x:int, y:int):
+        return self.op(x, y)
+    
 class Calculate():
-    def __init__(self, x:int, y:int, operation:Operation):
+    def __init__(self, x=None, y=None, operation:Operation=None):
         self.x = x
         self.y = y
         self.operation = operation
-        self.result = self.operation(x, y)
+        self.__is_finished = False
+
+    @property
+    def result(self):
+        result = None
+
+        if self.operation is not None:
+            if self.x is not None and self.y is not None:
+                self.__is_finished = True
+                result = self.operation.calculate(self.x, self.y)
+
+        return result
+        
+    @property
+    def status(self):
+        status = None
+
+        if self.x is None:
+            status = Status.EMPTY
+        elif self.operation is None:
+            status = Status.PARTIAL
+        elif self.y is None:
+            status = Status.PENDING
+        elif not self.__is_finished:
+            status = Status.COMPLETE
+        else:
+            status = Status.FINISHED
+
+        return status
--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,4 +1,4 @@
-from app.models.calculate import Operation, Calculate
+from app.models.calculate import Status, Operation, Calculate
 import pytest
 
 def test_calculate_add():
@@ -16,13 +16,13 @@ def test_calculate_subtract():
     assert calc.result == -1
 
 def test_calculate_multiplication():
-    calc = Calculate(4, 2, Operation.MUL)
+    calc = Calculate(4, 2, Operation.MULT)
     assert calc.x == 4
     assert calc.y == 2
-    assert calc.operation == Operation.MUL
+    assert calc.operation == Operation.MULT
     assert calc.result == 8
 
-    assert Calculate(0, 2, Operation.MUL).result == 0
+    assert Calculate(0, 2, Operation.MULT).result == 0
 
 def test_calculate_division():
     calc = Calculate(4, 2, Operation.DIV)
@@ -34,5 +34,42 @@ def test_calculate_division():
     assert Calculate(5, 2, Operation.DIV).result == 2.5
     assert Calculate(0, 4, Operation.DIV).result == 0
     with pytest.raises(ZeroDivisionError):
-        Calculate(4, 0, Operation.DIV)
+        Calculate(4, 0, Operation.DIV).result
 
+def test_calculate_incomplete():
+    calc = Calculate()
+    assert calc.x is None
+    assert calc.y is None
+    assert calc.operation is None
+    assert calc.result is None
+
+    assert calc.status == Status.EMPTY
+
+def test_calculate_with_x():
+    calc = Calculate(1)
+    assert calc.x == 1
+    assert calc.y is None
+    assert calc.operation is None
+    assert calc.result is None
+
+    assert calc.status == Status.PARTIAL
+
+def test_calculate_with_operation():
+    calc = Calculate(1, operation=Operation.ADD)
+    assert calc.x == 1
+    assert calc.y is None
+    assert calc.operation is Operation.ADD
+    assert calc.result is None
+
+    assert calc.status == Status.PENDING
+
+def test_calculate_complete():
+    calc = Calculate(1, 2, Operation.ADD)
+    assert calc.x == 1
+    assert calc.y == 2
+    assert calc.operation is Operation.ADD
+
+    assert calc.status == Status.COMPLETE
+
+    assert calc.result == 3
+    assert calc.status == Status.FINISHED
